a9d4d4fcb6f6aaaf41e458f607ecb09518471a8f
HARRiE_Dev <62064385+HARRiE13-Dev@users.noreply.github.com>
2021-10-01 14:00:37 +0700

    Final Day6P6

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
-use Illuminate\Routing\RouteGroup;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +13,7 @@ use Illuminate\Routing\RouteGroup;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -25,11 +23,11 @@ use Illuminate\Routing\RouteGroup;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-
 //PROTECTED Route::resource('user', UserController::class);
-Route::group(['middleware' => 'auth:sanctum'], function(){
+Route::group(['middleware' => 'auth:sanctum'], function ()
+{
     Route::resource('products', ProductController::class);
+    Route::get('products/search/{keyword}', [ProductController::class, 'search']);
     Route::post('logout', [AuthController::class, 'logout']);
-    
-});
 
+});
